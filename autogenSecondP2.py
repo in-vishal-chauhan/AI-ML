2704b23a834c7fa233d72649ae6cec428a8380bb
Paras-Tiez <paras.majethiya@tiezinteractive.com>
2025-04-18 12:31:23 +0530

    code fix

--- autogenSecondP2.py
+++ autogenSecondP2.py
@@ -1,10 +1,10 @@
 from autogen import ConversableAgent
 
 llm_config = {
-    "model": "llama-3.2-1b",
-    "base_url": "http://localhost:1234/v1",
+    "model": "gemma-2-2b-it",
+    "base_url": "http://127.0.0.1:1234/v1",
     "api_key": "lm-studio",
-    "price": [0,0],
+    "price": [0,0]
 }
 
 bret = ConversableAgent(
@@ -18,15 +18,10 @@ jemaine = ConversableAgent(
     llm_config=llm_config,
     system_message="Your name is Jemaine and you are a stand-up comedian in a two-person comedy show.",
 )
-print("Program started...")
+
 chat_result = bret.generate_reply(
     sender = jemaine,
-    message="Tell me a joke",
+    messages=[{"role": "user", "content": "Tell me a joke"}]
 )
-print("Program end...")
 
-print(len(chat_result))
-# print(chat_result.last_message()["content"])
-# for msg in chat_result.chat_history:
-#     print(msg.get("content", ""))
-    # chat result reply on whatsapp number
+print(chat_result)
