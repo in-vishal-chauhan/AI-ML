2eadc51a38b5ac3b85da422900602c02848f2e8f
Paras-Tiez <paras.majethiya@tiezinteractive.com>
2025-04-30 19:31:03 +0530

    remove lang

--- smart_receptionist/services/whisper_service.py
+++ smart_receptionist/services/whisper_service.py
@@ -7,7 +7,7 @@ model = WhisperModel("base", compute_type="float32")
 
 def transcribe_audio(file_path):
     try:
-        segments, info = model.transcribe(file_path, beam_size=5, language="en")
+        segments, info = model.transcribe(file_path, beam_size=5)
         transcript = " ".join([segment.text for segment in segments])
         return transcript, info.language
     except Exception as e:
