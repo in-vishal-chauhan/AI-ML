11da5d46b7dded8eed3bb8d8a9574a500ed25f71
Paras-Tiez <paras.majethiya@tiezinteractive.com>
2025-05-08 16:26:43 +0530

    docx read

--- smart_receptionist/services/read_store_vector.py
+++ smart_receptionist/services/read_store_vector.py
@@ -1,42 +1,37 @@
 import os
 import glob
 from docx import Document as DocxDocument
-import fitz
-import markdown
 from logger import get_logger
-from services.pinecone_service import PineconeService
-from config import Config
+import re
 
 logger = get_logger(__name__)
 
 class ReadStoreVector:
-    def __init__(self, documents_path="./documents/docx"):
+    def __init__(self, documents_path="./documents/docx", chunk_size=50):
         self.documents_path = documents_path
-
-    def _read_txt(self, path):
-        with open(path, "r", encoding="utf-8") as f:
-            return f.read()
+        self.chunk_size = chunk_size
 
     def _read_docx(self, path):
         doc = DocxDocument(path)
         return "\n".join(p.text for p in doc.paragraphs)
 
-    def _read_pdf(self, path):
-        doc = fitz.open(path)
-        return "\n".join([page.get_text() for page in doc])
+    def _chunk_text(self, text):
+        tokens = re.findall(r'\S+', text)
+        total_words = len(tokens)
+        chunks = []
 
-    def _read_md(self, path):
-        with open(path, "r", encoding="utf-8") as f:
-            raw_md = f.read()
-            return markdown.markdown(raw_md)
+        for i in range(0, total_words, self.chunk_size):
+            chunk = tokens[i:i + self.chunk_size]
+            chunks.append(" ".join(chunk))
+
+        return chunks
 
     def query(self):
         file_readers = {
-            ".txt": self._read_txt,
             ".docx": self._read_docx,
-            ".pdf": self._read_pdf,
-            ".md": self._read_md,
         }
+
+        all_chunks = []
 
         for ext, reader in file_readers.items():
             files = glob.glob(os.path.join(self.documents_path, f"**/*{ext}"), recursive=True)
@@ -48,11 +43,16 @@ class ReadStoreVector:
                         logger.error(f"Empty content in file: {file_path}")
                         continue
 
-                    file_name = os.path.splitext(os.path.basename(file_path))[0]
+                    chunks = self._chunk_text(content)
 
-                    record = [{"_id": file_name,"chunk_text": content}]
+                    for idx, chunk in enumerate(chunks):
+                        file_name = os.path.splitext(os.path.basename(file_path))[0]
+                        record = {"_id": f"{file_name}_chunk_{idx + 1}", "chunk_text": chunk}
+                        all_chunks.append(record)
 
-                    return record
                 except Exception as e:
                     logger.error(f"Failed to read file {file_path}: {e}")
                     continue
+        logger.error(all_chunks)
+        exit(0)
+        # return all_chunks
